bdb3257a226dc4209264f58b2403775884dbe1d3
jentlAntheunis <113892951+jentlAntheunis@users.noreply.github.com>
2023-10-14 17:32:30 +0200

    added update function

--- script/app.js
+++ script/app.js
@@ -1,32 +1,50 @@
 // _ = helper functions
-function _parseMillisecondsIntoReadableTime(timestamp) {
-	//Get hours from milliseconds
-	const date = new Date(timestamp * 1000);
-	// Hours part from the timestamp
-	const hours = '0' + date.getHours();
-	// Minutes part from the timestamp
-	const minutes = '0' + date.getMinutes();
-	// Seconds part from the timestamp (gebruiken we nu niet)
-	// const seconds = '0' + date.getSeconds();
-
-	// Will display time in 10:30(:23) format
-	return hours.substr(-2) + ':' + minutes.substr(-2); //  + ':' + s
-}
+const _formatTime = (time) => {
+	if (time < 10) {
+		return `0${time}`;
+	} else {
+		return `${time}`;
+	}
+};
 
 // 5 TODO: maak updateSun functie
+const updateSunPosition = (sunrise, sunset) => {
+	//get current time
+	const now = new Date();
+	//update minutes left
+	updateMinutesLeft(sunset, now);
+	//update sun position
+	placeSun(sunrise, sunset, now);
+};
 
 // 4 Zet de zon op de juiste plaats en zorg ervoor dat dit iedere minuut gebeurt.
-let placeSunAndStartMoving = (totalMinutes, sunrise) => {
-	// In de functie moeten we eerst wat zaken ophalen en berekenen.
-	// Haal het DOM element van onze zon op en van onze aantal minuten resterend deze dag.
-	// Bepaal het aantal minuten dat de zon al op is.
-	// Nu zetten we de zon op de initiële goede positie ( met de functie updateSun ). Bereken hiervoor hoeveel procent er van de totale zon-tijd al voorbij is.
-	// We voegen ook de 'is-loaded' class toe aan de body-tag.
-	// Vergeet niet om het resterende aantal minuten in te vullen.
-	// Nu maken we een functie die de zon elke minuut zal updaten
-	// Bekijk of de zon niet nog onder of reeds onder is
-	// Anders kunnen we huidige waarden evalueren en de zon updaten via de updateSun functie.
-	// PS.: vergeet weer niet om het resterend aantal minuten te updaten en verhoog het aantal verstreken minuten.
+const placeSun = (sunrise, sunset, now) => {
+	//calculate the percentage of the day that has passed
+	const totalMinutes = (sunset - sunrise) / 1000 / 60;
+	const minutesPassed = (now - sunrise) / 1000 / 60;
+	console.info(`${minutesPassed} minutes passed of ${totalMinutes} minutes`);
+	if (minutesPassed < totalMinutes && minutesPassed > 0) {
+		const percentage = (minutesPassed / totalMinutes) * 100;
+
+		//map percentage to rotation between 0 and 180 degrees
+		const rotation = (180 * percentage) / 100;
+
+		//rotate sun element
+		const sun = document.querySelector('.js-sun');
+		sun.style.transform = `rotate(${rotation}deg)`;
+		if (sun.style.display === 'none') {
+			sun.style.display = null;
+		};
+		console.info('sun is up');
+
+		setInterval(e => {
+			updateSunPosition(sunrise, sunset);
+		}, 60000);
+	} else {
+		console.info('sun is down');
+		document.querySelector('.js-time-left').innerHTML = '';
+		document.querySelector('.js-summary').innerHTML = 'The sun has is down, tommorow it will rise again.';
+	}
 };
 
 // 3 Met de data van de API kunnen we de app opvullen
@@ -39,45 +57,23 @@ let showResult = (queryResponse) => {
 	const sunrise = new Date(queryResponse.city.sunrise * 1000);
 	const sunset = new Date(queryResponse.city.sunset * 1000);
 	//format sunrise and sunset time to HH:MM
-	const sunriseTime = `${formatTime(sunrise.getHours())}:${formatTime(sunrise.getMinutes())}`;
-	const sunsetTime = `${formatTime(sunset.getHours())}:${formatTime(sunset.getMinutes())}`;
+	const sunriseTime = `${_formatTime(sunrise.getHours())}:${_formatTime(sunrise.getMinutes())}`;
+	const sunsetTime = `${_formatTime(sunset.getHours())}:${_formatTime(sunset.getMinutes())}`;
 	document.querySelector('.js-sunrise').innerHTML = sunriseTime;
 	document.querySelector('.js-sunset').innerHTML = sunsetTime;
 	//get current time
 	const now = new Date();
 	// Hier gaan we een functie oproepen die de zon een bepaalde positie kan geven en dit kan updaten.
-	updateSunPosition(sunrise, sunset, now);
+	placeSun(sunrise, sunset, now);
 
 	updateMinutesLeft(sunset, now);
 	// Geef deze functie de periode tussen sunrise en sunset mee en het tijdstip van sunrise.
-};
-
-const updateSunPosition = (sunrise, sunset, now) => {
-	//calculate the percentage of the day that has passed
-	const totalMinutes = (sunset - sunrise) / 1000 / 60;
-	const minutesPassed = (now - sunrise) / 1000 / 60;
-	const percentage = (minutesPassed / totalMinutes) * 100;
-
-	//map percentage to rotation between 0 and 180 degrees
-	const rotation = (180 * percentage) / 100;
-	
-	//rotate sun element
-	const sun = document.querySelector('.js-sun');
-	sun.style.transform = `rotate(${rotation}deg)`;
 };
 
 const updateMinutesLeft = (sunset, now) => {
 	const minutesLeft = (sunset - now) / 1000 / 60;
 	const minutes = Math.floor(minutesLeft);
 	document.querySelector('.js-time-left').innerHTML = `${minutes} minutes`;
-};
-
-const formatTime = (time) => {
-	if (time < 10) {
-		return `0${time}`;
-	} else {
-		return `${time}`;
-	}
 };
 
 // 2 Aan de hand van een longitude en latitude gaan we de yahoo wheater API ophalen.
